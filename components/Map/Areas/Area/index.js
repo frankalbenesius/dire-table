222a2a33fc7935496500e5dbdde7e6f4a550b7de
Frank Albenesius <frankalbenesius@gmail.com>
2016-11-30 21:28:08 -0600

    Add "holes" to areas, refactor path logic

--- components/Map/Areas/Area/index.js
+++ components/Map/Areas/Area/index.js
@@ -6,23 +6,38 @@ import { colors } from '../../../constants'
 // is the final svg px paths. Other map pieces will also need to know
 // how to make these final paths so no sense in putting the logic here.
 const Area = ({ coords, centerPx, cellSize }) => {
-  const coordsToPx = (points, center, unitSize) =>
-    points.map(coord =>
-      coord.map((distance, i) => {
-        const modifier = i === 1 ? -1 : 1
-        return center + (modifier * (distance * unitSize))
-      }))
-  const outlinePxCoords = coordsToPx(coords[0], centerPx, cellSize)
-
-  const outlinePoints = points => (
-    points.reduce((acc, point) => `${acc}${point[0]},${point[1]} `, '')
+  const toPxCoordinate = (center, unit) => gridCoordinate => (
+    gridCoordinate.map((position, i) => {
+      const modifier = i === 1 ? -1 : 1 // -1 on y, 1 on x
+      return center + (modifier * (position * unit))
+    })
   )
+  const toPxCoordinateSequence = (center, unit) => gridCoordinates => (
+    gridCoordinates.map(toPxCoordinate(center, unit))
+  )
+  const toPath = (pxCoordinateSequence) => {
+    const str = pxCoordinateSequence.reduce((acc, point, i) => {
+      const command = i === 0 ? 'M' : 'L'
+      return `${acc}${command} ${point[0]},${point[1]} `
+    }, '')
+    return `${str} Z `
+  }
+  const toCombinedPath = (gridCoordinateSequences, center, unit) => (
+    gridCoordinateSequences
+      .map(toPxCoordinateSequence(center, unit))
+      .map(toPath)
+      .join('')
+  )
+  const combinedPath = toCombinedPath(coords, centerPx, cellSize)
+
   return (
-    <polygon
-      points={outlinePoints(outlinePxCoords)}
+    <path
+      fillRule="evenodd"
+      d={combinedPath}
       fill={colors.foreground}
       stroke={colors.background}
-      strokeWidth="8px"
+      strokeLinecap="square"
+      strokeWidth="8"
     />
   )
 }
