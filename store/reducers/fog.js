72bc662bb127149e9c5c629482e1c3214d0232af
Frank Albenesius <frankalbenesius@gmail.com>
2016-12-02 01:20:12 -0600

    Add TODO for holes, multiple fog areas

--- store/reducers/fog.js
+++ store/reducers/fog.js
@@ -1,5 +1,6 @@
 const defaultState = [
   [[-4, -4], [-4, 5], [6, 5], [6, -4]],
+  // TODO: add support for fog holes & other multiple Fog areas
 ]
 
 export default function reducer(state = defaultState, action) {
