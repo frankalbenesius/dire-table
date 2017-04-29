efd65ee30de58a0960c51b9915001fbf01bed87b
Frank Albenesius <frankalbenesius@gmail.com>
2017-04-29 11:31:35 -0500

    change playerlist to Roster

---
+++ components/Roster/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from 'glamor';
+
+import { sizes } from '../constants';
+
+import Token from '../Token';
+
+const tokenSize = 50;
+
+const Roster = ({ roster, tokens, onPlayerClick }) => {
+  const playerList = Object.keys(roster).map(k => roster[k]);
+  const inventoryHeight = tokenSize * playerList.length;
+  const styles = {
+    wrapper: css({
+      position: 'absolute',
+      left: '0',
+      bottom: '0',
+      margin: sizes.overlayPadding,
+      height: `${inventoryHeight}px`,
+      pointerEvents: 'none',
+    }),
+    svg: css({
+      height: '100%',
+    }),
+    label: css({
+      fontFamily: 'Vulf Mono Bold',
+      fontSize: '0.8rem',
+      userSelect: 'none',
+    }),
+  };
+  return (
+    <div className={styles.wrapper}>
+      <svg className={styles.svg}>
+        {playerList.map((player, i) => {
+          let icon = 'smile';
+          const playerTokens = tokens.filter(t => t.player === player.id);
+          const playerHasAToken = playerTokens.length > 0;
+          if (playerHasAToken) {
+            icon = playerTokens.sort((a, b) => b.lastUpdated - a.lastUpdated)[0].icon;
+          }
+          return (
+            <g key={player.id}>
+              <Token
+                player={player.id}
+                icon={icon}
+                cx={tokenSize / 2}
+                cy={inventoryHeight - (tokenSize * i + tokenSize / 2)}
+                radius={25}
+                draggable={false}
+                dragging={false}
+                onMouseDown={() => onPlayerClick(player.id)}
+              />
+              <text
+                className={styles.label}
+                x={tokenSize + 5}
+                y={inventoryHeight - (tokenSize * i + tokenSize / 2)}
+                dy={4}
+                textAnchor={'start'}
+              >
+                {player.name}
+              </text>
+            </g>
+          );
+        })}
+      </svg>
+    </div>
+  );
+};
+Roster.propTypes = {
+  roster: PropTypes.object,
+  tokens: PropTypes.array,
+  onPlayerClick: PropTypes.func,
+};
+
+export default Roster;
